2c04d3a83618580e826e19098baef18b66441af4
Jack Nebel <jackwnebel@gmail.com>
2016-11-05 11:21:27 -0500

    refactor state creation in compiler

--- src/Compiler.ts
+++ src/Compiler.ts
@@ -1,64 +1,82 @@
-import {Split, Match, State} from "./State";
+import { Split, Match, State } from "./State";
 
-class Frag
-{
-    constructor(public start: State, public end: State[]){
+class Frag {
+    constructor(public start: State, public end: State[]) {
     }
 
-    public patch(input: State)
-    {
+    public patch(input: State) {
         this.end.forEach((outState) => {
             outState.out.push(input);
         });
     }
 }
 
-
-
-export function post2nfa(postfix: string) : State
-{
+export function post2nfa(postfix: string): State {
     var stack = [] as Frag[];
     postfix.split("").forEach((character) => {
-        let e1, e2, state, e;
-        switch(character)
-        {
+        switch (character) {
             case "|":
-                e2 = stack.shift();
-                e1 = stack.shift();
-                state = new State(Split, [e1.start, e2.start]);
-                stack.unshift(new Frag(state, e1.end.concat(e2.end)));
+                createAlternationState(stack);
                 break;
             case ".":
-                e2 = stack.shift();
-                e1 = stack.shift();
-                e1.patch(e2.start);
-                stack.unshift(new Frag(e1.start, e2.end));
+                createCatenationState(stack);
                 break;
             case "?":
-                e = stack.shift();
-                state = new State(Split, [e.start]);
-                stack.unshift(new Frag(state, e.end.concat(state)));
+                createZeroOrOneState(stack);
                 break;
             case "*":
-                e = stack.shift();
-                state = new State(Split, [e.start]);
-                e.patch(state);
-                stack.unshift(new Frag(state, [state]));
+                createZeroOrMoreState(stack);
                 break;
             case "+":
-                e = stack.shift();
-                state = new State(Split, [e.start]);
-                e.patch(state);
-                stack.unshift(new Frag(e.start, [state]));
+                createOneOrMoreState(stack);
                 break;
             default:
-                state = new State(character.charCodeAt(0), []);
-                stack.unshift(new Frag(state, [state]));
+                createCharacterMatchState(stack, character);
                 break;
         }
-      
+
     });
-      let e = stack.shift();
-      e.patch(new State(Match, []))
-      return e.start;
+    let e = stack.shift();
+    e.patch(new State(Match, []))
+    return e.start;
+}
+
+function createAlternationState(stack) {
+    let e2 = stack.shift();
+    let e1 = stack.shift();
+    let state = new State(Split, [e1.start, e2.start]);
+    stack.unshift(new Frag(state, e1.end.concat(e2.end)));
+}
+
+function createCatenationState(stack) {
+    let e2 = stack.shift();
+    let e1 = stack.shift();
+    e1.patch(e2.start);
+    stack.unshift(new Frag(e1.start, e2.end));
+}
+
+function createZeroOrOneState(stack) {
+    let e = stack.shift();
+    let state = new State(Split, [e.start]);
+    stack.unshift(new Frag(state, e.end.concat(state)));
+}
+
+function createZeroOrMoreState(stack) {
+    let e = stack.shift();
+    let state = new State(Split, [e.start]);
+    e.patch(state);
+    stack.unshift(new Frag(state, [state]));
+}
+
+function createOneOrMoreState(stack) {
+    let e = stack.shift();
+    let state = new State(Split, [e.start]);
+    e.patch(state);
+    stack.unshift(new Frag(e.start, [state]));
+}
+
+function createCharacterMatchState(stack, character) {
+    let state = new State(character.charCodeAt(0), []);
+    stack.unshift(new Frag(state, [state]));
+
 }
